7123b71a06d73d0c5b7423f4e15500c18735b3ab
ReedBelca <reedbelca55@gmail.com>
2026-05-02 23:30:11 +0000

    Creating Static Server

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 import file from "./file.js";
+import classes from "./style.module.css";
 
 console.log("Hello World!");
 console.log(file.value);
+
+document.querySelector("h1").classList.add(classes.title);
